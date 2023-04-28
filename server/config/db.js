f006c826b9b363f41c77ec46928fd8339d4a8304
AleBradC <alexandrabradclaudia@gmail.com>
2023-04-28 20:43:57 +0300

    improvements

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI);
-
-  console.log(`Mongo DB connected ${conn.connection.host}`.cyan.underline.bold);
+  mongoose.set("strictQuery", false);
+  await mongoose.connect(process.env.MONGO_URI, () => {
+    console.log("Mongo DB connected");
+  });
 };
 
 module.exports = connectDB;
